9799dcb64723520d69b3e5f4a2e5bbedb28826a2
DESKTOP-L8O2RPO\Lenovo <abdurrahim.3078@gmail.com>
2020-12-22 19:28:51 +0300

    comment page

--- Controllers/CitiesController.cs
+++ Controllers/CitiesController.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using cityGuide.Data;
 using cityGuide.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,6 +15,29 @@ namespace cityGuide.Controllers
         {
             _context = context;
         }
+
+        [HttpPost]
+        public async Task<IActionResult> NewComment(int id, Comment comment)
+        {
+            if (HttpContext.Session.GetString("isUserLogin") != null)
+            {
+                comment.CityId = id;
+                comment.UserId = int.Parse(HttpContext.Session.GetString("UserId"));
+                await _context.Comment.AddAsync(comment);
+                var result = await _context.SaveChangesAsync();
+                if (result > 0)
+                {
+                    return RedirectToAction("Details", "Cities", new {id});
+
+                }
+                return RedirectToAction("Details", "Cities",new {id});
+
+            }
+            else
+            {
+                return RedirectToAction("Details", "Cities", new {id});
+            }
+        }
         public async Task<IActionResult> Index()
         {
 
@@ -24,7 +48,7 @@ namespace cityGuide.Controllers
         public async Task<IActionResult> Details(int id)
         {
 
-            var city = await _context.City.Include(c => c.Comment).ThenInclude(y => y.User).FirstOrDefaultAsync(c =>c.CityId ==id );
+            var city = await _context.City.Include(c => c.Comment).ThenInclude(y => y.User).FirstOrDefaultAsync(c => c.CityId == id);
 
 
             return View(city);
